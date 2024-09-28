296978613e7ab9939e701af820d7e77947ccfdcd
pratiksha-more <pratikshamore077@gmail.com>
2024-09-28 17:22:25 +0530

    Create task and corresponding file with the task name

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const { log } = require("console");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -11,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
   fs.readdir(`./files`, function (err, files) {
-    console.log(files);
+    // console.log(files);
     res.render("index", { files: files });
   });
 });
@@ -25,6 +29,25 @@ app.post("/create", function (req, res) {
     }
   );
 });
+
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      // console.log(filedata);
+      res.render("showdata", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    }
+  );
+});
+
+app.get("/edit/:filename", function (req, res) {
+  res.render(`edit`, { filename: req.params.filename });
+});
+
 
 app.listen(5000, () => {
   console.log("Server start on port : 5000");
